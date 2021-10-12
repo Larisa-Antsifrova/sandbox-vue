a00ecb095e320de2886bb86fc5cc11cf2d454d4e
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-10-12 22:49:53 +0300

    Added PageNotFound component.

--- vue-course/src/router.js
+++ vue-course/src/router.js
@@ -1,10 +1,12 @@
 import VueRouter from 'vue-router';
 import HomePage from './pages/HomePage.vue';
 import ApartmentPage from './pages/ApartmentPage.vue';
+import ErrorPage from './pages/ErrorPage.vue';
 
 const routes = [
   { path: '/', component: HomePage, name: 'home' },
   { path: '/apartments/:id', component: ApartmentPage, name: 'apartment' },
+  { path: '*', component: ErrorPage, name: 'error-page' },
 ];
 
 const router = new VueRouter({ routes, mode: 'history' });
