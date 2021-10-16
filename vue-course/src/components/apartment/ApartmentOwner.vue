76c9795b05d07ef2c506317a407b594f322594b1
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-10-16 23:23:42 +0300

    Added apartment owner info.

---
+++ vue-course/src/components/apartment/ApartmentOwner.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="apartment-owner">
+    <h2 class="apartment-owner__title">Owner's Info:</h2>
+    <p class="apartment-owner__info">Name: {{ owner.name }}</p>
+    <p class="apartment-owner__info">Tel.: {{ owner.phone }}</p>
+    <p class="apartment-owner__info">Email: {{ owner.email }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ApartmentOwner',
+  props: {
+    owner: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/variables';
+
+.apartment-owner {
+  padding: 20px;
+  background: $card-bg;
+  font-weight: 400;
+
+  &__title {
+    font-size: 18px;
+    font-weight: 700;
+    margin-bottom: 20px;
+  }
+
+  &__info {
+    margin-top: 6px;
+  }
+}
+</style>
--- vue-course/src/pages/ApartmentPage.vue
+++ vue-course/src/pages/ApartmentPage.vue
@@ -1,7 +1,18 @@
 <template>
   <main class="apartment-page">
     <Container>
-      <ApartmentMainInfo :apartment="apartment" />
+      <div class="apartment-page__content">
+        <ApartmentMainInfo
+          :apartment="apartment"
+          class="apartment-page__main-info"
+        />
+        <div class="apartment-page__additional-info">
+          <ApartmentOwner
+            :owner="apartment.owner"
+            class="apartment-page__owner"
+          />
+        </div>
+      </div>
     </Container>
   </main>
 </template>
@@ -9,12 +20,13 @@
 <script>
 import Container from '../components/shared/Container.vue';
 import ApartmentMainInfo from '../components/apartment/ApartmentMainInfo.vue';
+import ApartmentOwner from '../components/apartment/ApartmentOwner.vue';
 
 import apartments from '../components/apartment/apartments';
 
 export default {
   name: 'ApartmentPage',
-  components: { Container, ApartmentMainInfo },
+  components: { Container, ApartmentMainInfo, ApartmentOwner },
   computed: {
     apartment() {
       return apartments.find(
@@ -31,15 +43,26 @@ export default {
 <style lang="scss" scoped>
 .apartment-page {
   padding-bottom: 55px;
+
   &__content {
     display: flex;
     align-items: flex-start;
   }
+
+  &__main-info {
+    width: 70%;
+  }
+
   &__additional-info {
     margin-left: 30px;
-    max-width: 350px;
+    min-width: 30%;
     flex-grow: 0;
     flex-shrink: 1;
+  }
+
+  &__owner {
+    min-width: 350px;
+    margin-left: 30px;
   }
 }
 </style>
