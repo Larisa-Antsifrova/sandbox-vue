ec239d28a9dbc89d88be0093c9e04d559f46ba51
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-10-17 10:54:50 +0300

    Added elements of the future review component.

---
+++ vue-course/src/components/reviews/reviews-item/ReviewsHeading.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="reviews-heading">
+    <ReviewsAvatar />
+    <div class="reviews-heading__content">
+      <p class="reviews-heading__author">{{ author }}</p>
+      <StarRating :rating="rating" />
+    </div>
+  </div>
+</template>
+
+<script>
+import StarRating from '../../StarRating.vue';
+import ReviewsAvatar from './ReviewsAvatar.vue';
+
+export default {
+  name: 'ReviewsHeading',
+  components: { StarRating, ReviewsAvatar },
+  props: {
+    author: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
