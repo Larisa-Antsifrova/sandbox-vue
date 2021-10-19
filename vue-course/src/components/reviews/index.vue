1166017f3fac1db3b2b0c4ca847d5dc0328dc27e
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-10-19 22:02:14 +0300

    Added revies section heading and more button.

--- vue-course/src/components/reviews/index.vue
+++ vue-course/src/components/reviews/index.vue
@@ -1,23 +1,47 @@
 <template>
   <section class="reviews">
+    <div class="reviews__heading">
+      <h2 class="reviews__title">Total Rating</h2>
+      <div class="reviews__rating">
+        <span>{{ amountOfReviews }} reviews</span>
+        <StarRating :rating="totalRating" />
+      </div>
+    </div>
+
     <ReviewsItem
       v-for="review in reviews"
       :key="review.author"
       :review="review"
     />
+
+    <button class="reviews__show-more">
+      Read more...
+    </button>
   </section>
 </template>
 
 <script>
 import ReviewsItem from './reviews-item';
+import StarRating from '../StarRating.vue';
 
 export default {
   name: 'Reviews',
-  components: { ReviewsItem },
+  components: { ReviewsItem, StarRating },
   props: {
     reviews: {
       type: Array,
       required: true,
+    },
+  },
+  computed: {
+    totalRating() {
+      const average =
+        this.reviews.reduce((total, review) => total + review.rating, 0) /
+        this.reviews.length;
+      return average;
+    },
+    amountOfReviews() {
+      return this.reviews.length;
     },
   },
 };
