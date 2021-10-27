e83b8e3be6f19df8335a9ee7bdacc213b32336f0
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-10-27 06:46:08 +0300

    Added files to prepare for provide-inject.

---
+++ vue-course/src/components/shared/form/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <form class="form">
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  provide() {
+    return {
+      form: this,
+    };
+  },
+  data() {
+    return {
+      inputs: [],
+    };
+  },
+  methods: {
+    registerInput(input) {
+      this.inputs.push(input);
+    },
+    unregisterInput(input) {
+      this.inputs.filter(item => item !== input);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue-course/src/pages/LoginPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="login-page"></main>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+};
+</script>
+
+<style lang="scss" scoped></style>
