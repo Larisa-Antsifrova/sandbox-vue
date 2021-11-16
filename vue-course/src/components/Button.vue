fd9b6a542cd43c84fa3f23d11fcef4ecfb27da64
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-11-16 06:52:43 +0200

    Added styles to loader.

--- vue-course/src/components/Button.vue
+++ vue-course/src/components/Button.vue
@@ -2,10 +2,13 @@
   <button
     v-on="$listeners"
     v-bind:type="type"
+    :disabled="loading"
     :class="{ btn: true, 'btn--outlined': outlined }"
   >
-    <CircleLoader :width="40" :height="40" />
-    <slot></slot>
+    <CircleLoader v-if="loading" :width="35" :height="35" class="btn__loader" />
+    <span class="btn__content" :class="contentStyles">
+      <slot></slot>
+    </span>
   </button>
 </template>
 
@@ -18,12 +21,21 @@ export default {
   props: {
     type: { type: String, default: 'button' },
     outlined: { type: Boolean, default: false },
+    loading: { type: Boolean, default: false },
+  },
+  computed: {
+    contentStyles() {
+      return {
+        'btn__content--hidden': this.loading,
+      };
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .btn {
+  position: relative;
   display: inline-block;
   min-width: 220px;
   width: 100%;
@@ -41,6 +53,18 @@ export default {
     color: #ff6620;
     background-color: #ffffff;
     border: 1px solid #ff6620;
+  }
+
+  &__content--hidden {
+    opacity: 0;
+  }
+
+  &__loader {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
   }
 }
 </style>
