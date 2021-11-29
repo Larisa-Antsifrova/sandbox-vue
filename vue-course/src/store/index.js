782903d4e182066ff90f10046e4b9f5ff8f947d7
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-11-29 06:53:15 +0200

    Added persisted state.

--- vue-course/src/store/index.js
+++ vue-course/src/store/index.js
@@ -1,13 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import authModule from './modules/auth';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
+
+const persistedstate = createPersistedState({
+  paths: ['auth.token'],
+});
 
 const store = new Vuex.Store({
   modules: {
     auth: authModule,
   },
+  plugins: [persistedstate],
 });
 
 export default store;
