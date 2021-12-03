68eb1cbd60881eec7e24cbaa1698eec9e562bcd8
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-12-03 06:53:14 +0200

    Added methods to manage profile menu.

--- vue-course/src/components/header/AccountActions.vue
+++ vue-course/src/components/header/AccountActions.vue
@@ -34,6 +34,22 @@
 <script>
 export default {
   name: 'AccountActions',
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    open() {
+      this.isOpen = true;
+    },
+    close() {
+      this.isOpen = false;
+    },
+    toggle() {
+      this.isOpen = !this.isOpen;
+    },
+  },
 };
 </script>
 
