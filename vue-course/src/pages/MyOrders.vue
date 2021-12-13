90b01b37a399add2f148508d9ad3b5bcdfa993d8
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-12-13 06:50:09 +0200

    Added basics of order page.

---
+++ vue-course/src/pages/MyOrders.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="my-orders-page">
+    <SectionWithHeaderSpacer>
+      <Container>
+        <section class="my-orders-page__content">
+          <h2>Orders</h2>
+        </section>
+      </Container>
+    </SectionWithHeaderSpacer>
+  </main>
+</template>
+
+<script>
+import SectionWithHeaderSpacer from '../components/shared/SectionWithHeaderSpacer.vue';
+import Container from '../components/shared/Container.vue';
+export default {
+  name: 'MyOrdersPage',
+  components: {
+    SectionWithHeaderSpacer,
+    Container,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
