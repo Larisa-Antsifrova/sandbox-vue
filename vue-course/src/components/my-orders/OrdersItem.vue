046a957d1ca9c8d4f981a76d3524ed5310cfa698
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-12-16 06:42:55 +0200

    Added basic order item markup.

--- vue-course/src/components/my-orders/OrdersItem.vue
+++ vue-course/src/components/my-orders/OrdersItem.vue
@@ -1,10 +1,28 @@
 <template>
-  <div></div>
+  <div class="orders-item">
+    <img class="orders-item__photo" :src="order.imgUrl" alt="Apartment photo" />
+    <div class="orders-item__details">
+      <div class="orders-item__text">
+        <h2 class="orders-item__title">{{ order.title }}</h2>
+        <p class="orders-item__location">{{ order.location.city }}</p>
+        <p class="orders-item__cost">
+          <span class="orders-item__price">UAH {{ order.price }}</span> per
+          night
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'OrdersItem',
+  props: {
+    order: {
+      type: Object,
+      required: true,
+    },
+  },
 };
 </script>
 
