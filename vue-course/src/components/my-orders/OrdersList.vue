a542de039ffc8de6f0abeda4b8377c0d3b83d73e
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-12-17 21:40:35 +0200

    Added Orders list component.

--- vue-course/src/components/my-orders/OrdersList.vue
+++ vue-course/src/components/my-orders/OrdersList.vue
@@ -1,10 +1,30 @@
 <template>
-  <div></div>
+  <div class="orders-list">
+    <p v-if="!items.length">No orders</p>
+    <template v-else>
+      <OrdersItem
+        v-for="item in items"
+        :order="item.apartment"
+        :key="item.id"
+      />
+    </template>
+  </div>
 </template>
 
 <script>
+import OrdersItem from './OrdersItem.vue';
+
 export default {
   name: 'OrdersList',
+  components: {
+    OrdersItem,
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
 };
 </script>
 
