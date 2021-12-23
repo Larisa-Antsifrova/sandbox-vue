5c6464f00619dc4ee0630a6b936390c28af678fa
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-12-23 20:31:49 +0200

    Added beforeEach router hook.

--- vue-course/src/router.js
+++ vue-course/src/router.js
@@ -5,6 +5,7 @@ import RegistrationPage from './pages/RegistrationPage.vue';
 import MyOrdersPage from './pages/MyOrdersPage.vue';
 import LoginPage from './pages/LoginPage.vue';
 import ErrorPage from './pages/ErrorPage.vue';
+import store from './store';
 
 const routes = [
   { path: '/', component: HomePage, name: 'home' },
@@ -43,6 +44,27 @@ const routes = [
   { path: '*', component: ErrorPage, name: 'error-page' },
 ];
 
-const router = new VueRouter({ routes, mode: 'history' });
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+});
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = store.getters['auth/isLoggedIn'];
+
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!isLoggedIn) {
+      next({ name: 'login-page' });
+    }
+  }
+
+  if (to.matched.some(record => record.meta.hideForAuth)) {
+    if (isLoggedIn) {
+      next({ name: 'home' });
+    }
+  }
+
+  next();
+});
 
 export default router;
