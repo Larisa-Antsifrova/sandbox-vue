97c8bc0b6e330b5f49f926b672853e1a248a66ff
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-01-03 22:28:54 +0200

    Added routes lazy loading.

--- vue-routing/src/router/index.js
+++ vue-routing/src/router/index.js
@@ -1,9 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
-import Brazil from '../views/Brazil.vue';
-import Hawaii from '../views/Hawaii.vue';
-import Panama from '../views/Panama.vue';
 
 Vue.use(VueRouter);
 
@@ -11,31 +7,31 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
   {
     path: '/brazil',
     name: 'brazil',
-    component: Brazil,
+    component: () =>
+      import(/* webpackChunkName: "brazil" */ '../views/Brazil.vue'),
   },
   {
     path: '/hawaii',
     name: 'hawaii',
-    component: Hawaii,
+    component: () =>
+      import(/* webpackChunkName: "hawaii" */ '../views/Hawaii.vue'),
   },
   {
     path: '/panama',
     name: 'panama',
-    component: Panama,
+    component: () =>
+      import(/* webpackChunkName: "panama" */ '../views/Panama.vue'),
   },
 ];
 
