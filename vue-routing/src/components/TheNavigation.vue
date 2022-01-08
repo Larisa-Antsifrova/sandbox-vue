7787363d2e513ee030fcc0eb466f263b3ff137bc
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-01-08 21:57:15 +0200

    Made nab component dynamic.

--- vue-routing/src/components/TheNavigation.vue
+++ vue-routing/src/components/TheNavigation.vue
@@ -1,30 +1,76 @@
 <template>
   <nav id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
-    <router-link to="/panama">Panama</router-link>
+    <p class="logo">Vue Router</p>
+
+    <ul class="nav-links">
+      <li class="links">
+        <router-link to="/">Home</router-link>
+      </li>
+
+      <li
+        v-for="destination in destinations"
+        :key="destination.name"
+        class="links"
+      >
+        <router-link :to="{ name: 'details', params: { id: destination.id } }">
+          {{ destination.name }}
+        </router-link>
+      </li>
+    </ul>
   </nav>
 </template>
 
 <script>
-export default {};
+import store from '@/store.js';
+
+export default {
+  name: 'TheNavigation',
+  data() {
+    return {
+      destinationId: this.$route.params.id,
+      destinations: store.destinations,
+    };
+  },
+};
 </script>
 
 <style scoped>
 #nav {
   display: flex;
-  justify-content: center;
-  padding: 30px;
+  align-items: center;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  border-bottom: 1px solid grey;
+  z-index: 1;
 }
 
 #nav a {
-  font-weight: bold;
   color: #2c3e50;
-  padding: 0 10px;
+  text-decoration: none;
+  font-weight: bold;
 }
 
 #nav a.navigation-test-active-class {
-  color: green;
+  color: #ab26ab;
+}
+
+.nav-links {
+  display: flex;
+}
+
+.links {
+  padding-right: 20px;
+  list-style: none;
+}
+
+.links:hover {
+  text-decoration: underline;
+}
+
+.logo {
+  font-size: 20px;
+  color: purple;
+  font-weight: bold;
 }
 </style>
