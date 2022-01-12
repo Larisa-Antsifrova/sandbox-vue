ae3107ab79ee53fd9efa5ef89eaeed770774b01d
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-01-12 20:34:50 +0200

    Added markup for experience details page.

---
+++ vue-routing/src/views/ExperienceDetails.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <h1>Experiences</h1>
+    <h2>{{ experience.name }}</h2>
+
+    <div class="experience-details">
+      <img
+        :src="`require(@/assets/${experience.image})`"
+        :alt="experience.name"
+      />
+
+      <p>{{ experience.description }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
