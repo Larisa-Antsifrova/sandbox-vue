0611348cd03b4a4abd3aad9c7646ab4cfe7080f9
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-01-28 06:49:18 +0200

    Added logout btn.

--- vue-routing/src/views/User.vue
+++ vue-routing/src/views/User.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
-    <h1>User Page</h1>
+    <h1>Welcome {{ user }}</h1>
+    <button @click="logOut" class="btn">Log out</button>
   </div>
 </template>
 
 <script>
-export default {};
+import store from "@/store";
+
+export default {
+  data() {
+    return {
+      user: store.user,
+    };
+  },
+  methods: {
+    logOut() {
+      store.user = null;
+      this.$router.push("/");
+    },
+  },
+};
 </script>
 
 <style scoped></style>
