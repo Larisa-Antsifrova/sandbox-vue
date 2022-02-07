85e662a84405d8e2bc85d8fe92d2bd65f682a125
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-02-07 06:51:40 +0200

    Started a new component test.

---
+++ vue-unit-testing/tests/unit/RandomNumber.spec.js
@@ -0,0 +1,8 @@
+import RandomNumber from "@/components/RandomNumber.vue";
+import { mount } from "@vue/test-utils";
+
+describe("RandomNumber", () => {
+  test("By default, random number should be 0", () => {
+    //
+  });
+});
