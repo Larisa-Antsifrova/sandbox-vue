651fb4491b94b0abbd6ba82d8e1aeb26eada1aff
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-02-08 06:51:49 +0200

    Added sanity check tests.

--- vue-unit-testing/tests/unit/RandomNumber.spec.js
+++ vue-unit-testing/tests/unit/RandomNumber.spec.js
@@ -3,6 +3,14 @@ import { mount } from "@vue/test-utils";
 
 describe("RandomNumber", () => {
   test("By default, random number should be 0", () => {
-    //
+    expect(true).toBe(false);
+  });
+
+  test("If button is clicked, the random number should be between 1 and 10", () => {
+    expect(true).toBe(false);
+  });
+
+  test("If button is clicked, the random number should be between 200 and 300", () => {
+    expect(true).toBe(false);
   });
 });
