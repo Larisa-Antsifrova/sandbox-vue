a48bd9e70de48897c5563451d200b9e5851d92f9
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-02-10 07:28:50 +0200

    Made second test pass.

--- vue-unit-testing/tests/unit/RandomNumber.spec.js
+++ vue-unit-testing/tests/unit/RandomNumber.spec.js
@@ -8,8 +8,14 @@ describe("RandomNumber", () => {
     expect(wrapper.html()).toContain("<span>0</span>");
   });
 
-  test("If button is clicked, the random number should be between 1 and 10", () => {
-    expect(true).toBe(false);
+  test("If button is clicked, the random number should be between 1 and 10", async () => {
+    const wrapper = mount(RandomNumber);
+
+    await wrapper.find("button").trigger("click");
+    const randomNumber = parseInt(wrapper.find("span").text());
+
+    expect(randomNumber).toBeGreaterThanOrEqual(1);
+    expect(randomNumber).toBeLessThanOrEqual(10);
   });
 
   test("If button is clicked, the random number should be between 200 and 300", () => {
