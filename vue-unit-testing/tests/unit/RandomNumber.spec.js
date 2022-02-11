20f1e639c8bfea44a7d89c6aba73bdfa7171154d
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-02-11 06:47:31 +0200

    Made third test pass.

--- vue-unit-testing/tests/unit/RandomNumber.spec.js
+++ vue-unit-testing/tests/unit/RandomNumber.spec.js
@@ -18,7 +18,18 @@ describe("RandomNumber", () => {
     expect(randomNumber).toBeLessThanOrEqual(10);
   });
 
-  test("If button is clicked, the random number should be between 200 and 300", () => {
-    expect(true).toBe(false);
+  test("If button is clicked, the random number should be between 200 and 300", async () => {
+    const wrapper = mount(RandomNumber, {
+      props: {
+        min: 200,
+        max: 300,
+      },
+    });
+
+    await wrapper.find("button").trigger("click");
+    const randomNumber = parseInt(wrapper.find("span").text());
+
+    expect(randomNumber).toBeGreaterThanOrEqual(200);
+    expect(randomNumber).toBeLessThanOrEqual(300);
   });
 });
