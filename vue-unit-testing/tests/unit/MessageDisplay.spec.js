0ccd51f4fa65b8e115095628b8a3871bd675c0a5
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-02-16 21:53:55 +0200

    Added test boilderplate.

---
+++ vue-unit-testing/tests/unit/MessageDisplay.spec.js
@@ -0,0 +1,12 @@
+import MessageDisplay from "@/components/MessageDisplay";
+import { mount } from "@vue/test-utils";
+
+describe("MessageDisplay", () => {
+  test("Calls getMessage and displays message", async () => {
+    // test goes here
+  });
+
+  test("Displays an error when getMessage call fails", async () => {
+    // test goes here
+  });
+});
