db14eb9d77a049c06ecffc74a8fd07073ac1c9fb
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-02-17 06:49:56 +0200

    Mocked axios module.

--- vue-unit-testing/tests/unit/MessageDisplay.spec.js
+++ vue-unit-testing/tests/unit/MessageDisplay.spec.js
@@ -1,9 +1,14 @@
 import MessageDisplay from "@/components/MessageDisplay";
 import { mount } from "@vue/test-utils";
+import { getMessage } from "@/services/axios";
+
+jest.mock("@/services/axios");
 
 describe("MessageDisplay", () => {
   test("Calls getMessage and displays message", async () => {
-    // test goes here
+    const mockMessage = "Hello from the db!";
+    getMessage.mockResolvedValueOnce({ text: mockMessage });
+    const wrapper = mount(MessageDisplay);
   });
 
   test("Displays an error when getMessage call fails", async () => {
