b99d1ed26d52c2bf79e249751e85515a2ae93619
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-02-19 21:34:52 +0200

    Added one more test.

--- vue-unit-testing/tests/unit/MessageDisplay.spec.js
+++ vue-unit-testing/tests/unit/MessageDisplay.spec.js
@@ -1,6 +1,7 @@
-import MessageDisplay from "@/components/MessageDisplay";
+import MessageDisplay from "@/components/MessageDisplay.vue";
 import { mount } from "@vue/test-utils";
 import { getMessage } from "@/services/axios";
+import flushPromises from "flush-promises";
 
 jest.mock("@/services/axios");
 
@@ -9,6 +10,14 @@ describe("MessageDisplay", () => {
     const mockMessage = "Hello from the db!";
     getMessage.mockResolvedValueOnce({ text: mockMessage });
     const wrapper = mount(MessageDisplay);
+
+    await flushPromises();
+
+    expect(getMessage).toHaveBeenCalledTimes(1);
+
+    const message = wrapper.find('[data-testid="message"]').text();
+
+    expect(message).toEqual(mockMessage);
   });
 
   test("Displays an error when getMessage call fails", async () => {
