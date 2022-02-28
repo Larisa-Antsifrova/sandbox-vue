4e35220dbb8997c55b61767bbe4ed70b3d617834
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-02-28 21:55:12 +0200

    Props compile micro.

--- vue-3-ts/src/App.vue
+++ vue-3-ts/src/App.vue
@@ -1,43 +1,23 @@
 <script setup lang="ts">
-import { ref, reactive, computed } from "vue";
+import { reactive } from "vue";
+import Counter from "./components/Counter.vue";
 
 interface AppInfo {
   name: string;
   moto: string;
 }
 
-const count = ref(0);
-
 const appInfo: AppInfo = reactive({
   name: "Counter",
   moto: "An app you can count on!",
 });
-
-// Example of infered types based on returned values
-const nextCount = computed(() => {
-  if (count.value !== null) {
-    return count.value + 1;
-  }
-
-  return null;
-});
-
-// Example of implicit type reference
-function addCount(number: number) {
-  if (count.value !== null) {
-    count.value += number;
-  }
-}
 </script>
 
 <template>
   <h1>{{ appInfo.name }}</h1>
   <h1>{{ appInfo.moto }}</h1>
 
-  <p>{{ count }}</p>
-  <div>
-    <button @click="addCount(1)">Add</button>
-  </div>
+  <Counter :limit="10" />
 </template>
 
 <style>
---
+++ vue-3-ts/src/components/Counter.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface Props {
+  limit: number;
+  messageOnLimit?: string;
+}
+
+// defineProps - compile micro
+// 1. Executes in compile-time
+// 2. No need to be imported
+// withDefaults - to provide some props default values
+const props = withDefaults(defineProps<Props>(), {
+  messageOnLimit: "Yep, that is the limit!",
+});
+
+const count = ref(0);
+
+function addCount(number: number) {
+  if (count.value !== null) {
+    if (count.value >= props.limit) {
+      alert(props.messageOnLimit);
+    } else {
+      count.value += number;
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p>{{ count }}</p>
+    <div>
+      <button class="btn" @click="addCount(1)">Add</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.btn {
+  min-width: 80px;
+  padding: 8px 12px;
+  background-color: tomato;
+  border: 1px solid transparent;
+  border-radius: 8px;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 700;
+  cursor: pointer;
+
+  transition: all 250ms ease-out;
+}
+
+.btn:hover {
+  color: tomato;
+  border: 1px solid tomato;
+  background-color: #fff;
+
+  transition: all 250ms ease-in;
+}
+</style>
