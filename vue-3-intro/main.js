12f25b16dd2eb613b8c15ab3be2ccc9b7abe2d92
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-03-08 19:21:09 +0200

    Rendered list of colors.

--- vue-3-intro/main.js
+++ vue-3-intro/main.js
@@ -7,6 +7,16 @@ const app = Vue.createApp({
       inventory: 100,
       onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+        },
+        {
+          id: 2235,
+          color: "blue",
+        },
+      ],
     };
   },
 });
