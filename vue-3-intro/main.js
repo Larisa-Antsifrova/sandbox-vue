5c3765aaa25a75a249bd4f736ca49eedf3564544
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-03-14 10:04:23 +0200

    Edited computed properties for variants.

--- vue-3-intro/main.js
+++ vue-3-intro/main.js
@@ -4,21 +4,21 @@ const app = Vue.createApp({
       cart: 0,
       product: "Socks",
       description: "Awesome socks to keep your feet warm and trendy!",
-      image: "./assets/images/socks_green.jpg",
+      selectedVariant: 0,
       brand: "Vue Mastery",
-      inventory: 100,
-      onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
         {
           id: 2234,
           color: "green",
           image: "./assets/images/socks_green.jpg",
+          quantity: 50,
         },
         {
           id: 2235,
           color: "blue",
           image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
         },
       ],
       sizes: ["S", "M", "L", "XL"],
@@ -26,18 +26,21 @@ const app = Vue.createApp({
   },
   computed: {
     inStock() {
-      return !!this.inventory;
+      return this.variants[this.selectedVariant].quantity;
     },
     title() {
       return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
     },
   },
   methods: {
     addToCart() {
       this.cart += 1;
     },
-    updateImage(image) {
-      this.image = image;
+    updateVariant(index) {
+      this.selectedVariant = index;
     },
   },
 });
