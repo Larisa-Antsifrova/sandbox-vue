2e9d6ba14c830c053d9c5acebef9179678797b0c
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-03-18 17:25:41 +0200

    Added products to cart.

--- vue-3-intro/main.js
+++ vue-3-intro/main.js
@@ -1,13 +1,13 @@
 const app = Vue.createApp({
   data() {
     return {
-      cart: 0,
+      cart: [],
       premium: false,
     };
   },
   methods: {
-    updateCart() {
-      this.cart += 1;
+    updateCart(productId) {
+      this.cart.push(productId);
     },
   },
 });
