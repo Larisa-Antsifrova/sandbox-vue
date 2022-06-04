3ba84b9d5f2264a478e21826781234f12bab94ca
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-06-04 22:05:22 +0300

    Added basic toolbar.

---
+++ vue-vuetify/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar flat app>
+    <v-app-bar-title class="text-uppercase grey--text">
+      <span class="font-weight-light">Todo</span>
+      <span>Ninja</span>
+    </v-app-bar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn text color="grey">
+      <span>Sign out</span>
+      <v-icon right>mdi-exit-to-app</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
