665f9e93f12dd1091312ff5850f42b8b43fce307
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-06-05 18:41:37 +0300

    Added drawer nav bar.

--- vue-vuetify/src/components/Navbar.vue
+++ vue-vuetify/src/components/Navbar.vue
@@ -1,19 +1,33 @@
 <template>
-  <v-app-bar flat app>
-    <v-app-bar-title class="text-uppercase grey--text">
-      <span class="font-weight-light">Todo</span>
-      <span>Ninja</span>
-    </v-app-bar-title>
+  <nav>
+    <v-app-bar flat app>
+      <v-app-bar-nav-icon class="gray--text" @click="drawer = !drawer">
+      </v-app-bar-nav-icon>
+      <v-app-bar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-app-bar-title>
 
-    <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-    <v-btn text color="grey">
-      <span>Sign out</span>
-      <v-icon right>mdi-exit-to-app</v-icon>
-    </v-btn>
-  </v-app-bar>
+      <v-btn text color="grey">
+        <span>Sign out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app class="indigo">
+      <p>Test</p>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
 </script>
