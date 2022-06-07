d7c0938acb79b2483cb7ef098d50832d8c6f3d0f
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-06-07 07:57:13 +0300

    Added links to navbar.

--- vue-vuetify/src/components/Navbar.vue
+++ vue-vuetify/src/components/Navbar.vue
@@ -17,7 +17,23 @@
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" app class="primary">
-      <p>Test</p>
+      <v-list>
+        <v-list-item
+          v-for="link in links"
+          :key="link.text"
+          router
+          :to="link.route"
+        >
+          <v-list-item-icon>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">
+              {{ link.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -27,6 +43,11 @@ export default {
   data() {
     return {
       drawer: false,
+      links: [
+        { icon: "mdi-dashboard", text: "Dashboard", route: "/" },
+        { icon: "mdi-folder", text: "My Projects", route: "/projects" },
+        { icon: "mdi-person", text: "Team", route: "/team" },
+      ],
     };
   },
 };
