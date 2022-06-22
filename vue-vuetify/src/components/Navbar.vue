29a00f52aead333bf539633f93f70bdb0e9b2b8b
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-06-22 23:08:47 +0300

    Added avatar to navbar.

--- vue-vuetify/src/components/Navbar.vue
+++ vue-vuetify/src/components/Navbar.vue
@@ -17,6 +17,15 @@
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" app class="primary">
+      <v-layout column align-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img src="/avatar-1.png" alt="avatar" />
+          </v-avatar>
+
+          <p class="white--text subheading mt-1">The Net Ninja</p>
+        </v-flex>
+      </v-layout>
       <v-list>
         <v-list-item
           v-for="link in links"
